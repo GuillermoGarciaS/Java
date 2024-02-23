662164f886f45d7afafa9723cf963210c39b5bbd
Guillermo Garcia <eivengamer11roblox@gmail.com>
2024-02-23 03:38:12 -0600

    third commit

---
+++ Repaso/nao.java
@@ -0,0 +1,35 @@
+package Repaso;
+
+import java.util.Scanner;
+
+public class nao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("================================");
+        
+        for (int i = 0; i < 3; i++) {
+            
+            String s1 = sc.next();
+            int x = sc.nextInt();
+
+
+            while (s1.length() > 10) {
+                
+                System.out.println("Your language name is too long, please enter again:");
+                s1 = sc.next();
+                
+            }
+
+            String formattedNumber = String.format("%03d", x);
+
+            System.out.printf("%-15s%s\n", s1, formattedNumber);
+        }
+        
+        System.out.println("================================");
+
+        sc.close();
+        
+    }
+
+}
